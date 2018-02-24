a22a4cf5f936530a60915ce67f47237d4f2f9218
FuckPetroKit <lad03069@kuiqa.com>
2018-02-24 18:59:59 +0100

    v3

--- fuckpetrov3.js
+++ fuckpetrov3.js
@@ -1,10 +1,9 @@
 var webPage = require('webpage');
 var page = webPage.create();
-
+var grecaptcha = new Object();
 page.open('https://regpetro.mppeuct.gob.ve/natural', function (status) {
   var content = page.content;
  //console.log('Content: ' + content);
- if (page.injectJs('petro.js')) {
   page.evaluate(function() {
    function rellenar() {
         min = 1;
@@ -12,29 +11,26 @@ page.open('https://regpetro.mppeuct.gob.ve/natural', function (status) {
         var grecaptcha = 2;
 	     grecaptcha.getResponse=function getResponse(){return 1};
         $("#t_dni").val('2');
+        $("#paises").val('189');
         $("#nombre").val(Math.floor(Math.random()*(max-min+1)+min)+' '+Math.floor(Math.random()*(max-min+1)+min));
         $("#email").val(Math.floor(Math.random()*(max-min+1)+min)+'@gmail.com');
         $("#dni").val(Math.floor(Math.random()*(max-min+1)+min));
         $('#registrar').submit();
 
-      
-     
-       
   }
 
     rellenar();
 
     console.log(document.title);
   });
-}
+
 var min=1000;
   var max=1000000000000;
 page.render('fillPetro'+Math.floor(Math.random()*(max-min+1)+min)+'.jpeg');
   setTimeout(function(){
        page.render('petro'+Math.floor(Math.random()*(max-min+1)+min)+'.jpeg'); 
-      // console.log(r1);
   phantom.exit();
-    }, 3000);
+    }, 2000);
 
 });
 
